1945d3194a6b2093af565aea96de83d84253f263
Shiwangi Yadav <shiwangiyadav001@gmail.com>
2021-08-25 22:51:20 +0530

    updated

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '6125305f0a8bc9023091d67f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
